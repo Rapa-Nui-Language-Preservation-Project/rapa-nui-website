d4685184c45ae952d075b448b2a779e5f044b7a7
Lucas Levine <lucaslevine12@gmail.com>
2025-04-08 21:37:55 -0400

    starting map implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<main
+  class="flex flex-col items-center justify-center min-h-screen p-4 bg-neutral-700 text-neutral-100"
+>
+  <h1 class="text-4xl">Rapa Nui</h1>
+  <div class="flex flex-row gap-4">
+    <a href="/locations" class="underline">Locations API Test</a>
+    <a href="/map" class="underline">Map Test</a>
+  </div>
+</main>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import maplibregl from "maplibre-gl";
+  import "maplibre-gl/dist/maplibre-gl.css";
+
+  $effect(() => {
+    const map = new maplibregl.Map({
+      container: "map",
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [-109.35, -27.15],
+      //   center: [0, 0],
+      zoom: 11,
+    });
+  });
+
+  let map_hidden = $state(false);
+</script>
+
+<div id="map" class="h-screen absolute" hidden={map_hidden}></div>
+<main class="flex justify-center w-screen h-screen bg-[#625661]">
+  <button
+    onclick={() => (map_hidden = !map_hidden)}
+    class="absolute right-0 top-0 bg-neutral-400 hover:bg-neutral-600 text-white p-2 m-4 rounded-lg cursor-pointer"
+  >
+    {map_hidden ? "Show Map" : "Hide Map"}</button
+  >
+  <div class="h-screen absolute">
+    <img
+      src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/6jjx168s5ezt2m8/map_k7mm569qll.png"
+      class="h-screen"
+      alt="Map of Easter Island"
+    />
+  </div>
+</main>
