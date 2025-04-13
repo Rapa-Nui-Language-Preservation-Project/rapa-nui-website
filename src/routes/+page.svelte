219cd06f8753a34c78e5f5592a76663b3f546bd4
Lucas Levine <lucaslevine12@gmail.com>
2025-04-13 14:18:22 -0400

    prettier & eslint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <main
-  class="flex flex-col items-center justify-center min-h-screen p-4 bg-neutral-700 text-neutral-100"
+	class="flex min-h-screen flex-col items-center justify-center bg-neutral-700 p-4 text-neutral-100"
 >
-  <h1 class="text-4xl">Rapa Nui</h1>
-  <div class="flex flex-row gap-4">
-    <a href="/locations" class="underline">Locations API Test</a>
-    <a href="/map" class="underline">Map Test</a>
-  </div>
+	<h1 class="text-4xl">Rapa Nui</h1>
+	<div class="flex flex-row gap-4">
+		<a href="/locations" class="underline">Locations API Test</a>
+		<a href="/map" class="underline">Map Test</a>
+	</div>
 </main>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,33 +1,50 @@
 <script>
-  import maplibregl from "maplibre-gl";
-  import "maplibre-gl/dist/maplibre-gl.css";
+	import maplibregl from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
 
-  $effect(() => {
-    const map = new maplibregl.Map({
-      container: "map",
-      style: "https://demotiles.maplibre.org/style.json",
-      center: [-109.35, -27.15],
-      //   center: [0, 0],
-      zoom: 11,
-    });
-  });
+	$effect(() => {
+		const map = new maplibregl.Map({
+			container: 'map',
+			style: 'https://demotiles.maplibre.org/style.json',
+			// style: "https://tiles.versatiles.org/assets/styles/colorful.json",
+			center: [-109.35, -27.15],
+			zoom: 11,
+			maxZoom: 20,
+			minZoom: 11,
+			maxBounds: [
+				[-109.65, -27.3], // Southwest coordinates
+				[-109, -26.96] // Northeast coordinates
+			]
+		});
 
-  let map_hidden = $state(false);
+		const marker = new maplibregl.Marker()
+			.setLngLat([-109.35961303710897, -27.116696224608503])
+			.addTo(map);
+
+		map.on('click', (e) => {
+			const coordinates = e.lngLat;
+			console.log('Clicked coordinates: ', coordinates);
+			marker.setLngLat(coordinates);
+		});
+	});
+
+	let map_hidden = $state(false);
 </script>
 
-<div id="map" class="h-screen absolute" hidden={map_hidden}></div>
-<main class="flex justify-center w-screen h-screen bg-[#625661]">
-  <button
-    onclick={() => (map_hidden = !map_hidden)}
-    class="absolute right-0 top-0 bg-neutral-400 hover:bg-neutral-600 text-white p-2 m-4 rounded-lg cursor-pointer"
-  >
-    {map_hidden ? "Show Map" : "Hide Map"}</button
-  >
-  <div class="h-screen absolute">
-    <img
-      src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/6jjx168s5ezt2m8/map_k7mm569qll.png"
-      class="h-screen"
-      alt="Map of Easter Island"
-    />
-  </div>
+<div id="map" class="absolute h-screen" hidden={map_hidden}></div>
+<main class="flex h-screen w-screen justify-center bg-[#625661]">
+	<button
+		onclick={() => (map_hidden = !map_hidden)}
+		class="absolute top-0 right-0 m-4 cursor-pointer rounded-lg bg-neutral-400 p-2 text-white hover:bg-neutral-600"
+	>
+		{map_hidden ? 'Show Map' : 'Hide Map'}</button
+	>
+	<div class="absolute h-screen">
+		<img
+			hidden={!map_hidden}
+			src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/6jjx168s5ezt2m8/map_k7mm569qll.png"
+			class="h-screen"
+			alt="Map of Easter Island"
+		/>
+	</div>
 </main>
