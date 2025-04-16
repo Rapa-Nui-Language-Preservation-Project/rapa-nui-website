aa0b29050cb7eeb2e0c13fc5b927f0558d2eb753
Lucas Levine <lucaslevine12@gmail.com>
2025-04-16 14:30:12 -0400

    header curr page underline

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import { Button } from './ui/button';
+
+	let currentPath = $state(page.url.pathname);
+
+	$effect(() => {
+		currentPath = page.url.pathname;
+	});
+
+	const pages = [
+		{ name: 'Home', path: '/' },
+		{ name: 'Map', path: '/map' }
+	];
 </script>
 
 <header class="absolute top-0 z-50 flex w-screen items-center p-5">
@@ -12,12 +24,13 @@
 		</div>
 	</div>
 	<div class="inline-flex shrink-0 items-center rounded-lg bg-secondary">
-		<a href="/">
-			<Button variant="link">Home</Button>
-		</a>
-		<a href="/map">
-			<Button variant="link">Map</Button>
-		</a>
+		{#each pages as page}
+			<a href={page.path} class="rounded-lg text-sm font-medium text-muted hover:bg-secondary">
+				<Button variant="link" class={page.path == currentPath ? 'underline' : ''}>
+					{page.name}
+				</Button>
+			</a>
+		{/each}
 	</div>
 	<div class="inline-flex justify-end" style="width: 50%;">
 		<ModeSwitcher />
