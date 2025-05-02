7eae4a6294ec767a056ec04f877a5559c56c1a98
Max_Admin <max_admin@aneslap26.local>
2025-05-01 21:34:50 -0400

    edits to homepage

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,13 +1,5 @@
 <footer class="text-faded relative bottom-0 m-4 rounded-lg">
-	<div class="flex w-screen flex-row items-center justify-center">
-		<span class="text-sm sm:text-center">© 2025. All Rights Reserved. </span>
-		<ul class="ml-4 flex flex-wrap items-center self-end text-sm">
-			<li>
-				<a href="/#" class="me-4 hover:underline md:me-6">Licensing</a>
-			</li>
-			<li>
-				<a href="/#" class="hover:underline">Contact</a>
-			</li>
-		</ul>
+	<div class="flex w-screen flex-row items-center justify-left text-xs xs:text-left">
+		© 2025. All Rights Reserved.
 	</div>
 </footer>
