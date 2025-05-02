58332e177725d8dfccc4006f0f3405adb5f4b816
Lucas Levine <lucaslevine12@gmail.com>
2025-05-01 21:46:38 -0400

    image size 75%

--- src/lib/components/map/DialogContent.svelte
+++ src/lib/components/map/DialogContent.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { MapPin } from 'lucide-svelte';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
 	import type { CarouselAPI } from '$lib/components/ui/carousel/context.js';
@@ -46,68 +47,75 @@
 		</Tooltip.Provider>
 	</Dialog.Trigger>
 	<Dialog.Content class="max-w-screen m-0 h-[90vh] w-[90vw]">
-		<div class="flex flex-col items-center">
-			<h1 class="text-xl font-bold">{location.name}</h1>
-			<p class="text-md font-normal">{location.description}</p>
-			{#if location.expand.media?.length == 1}
-				<div class="flex flex-col items-center justify-center text-center">
-					<h2 class="text-md m-2">Media</h2>
-					<img src={location.expand.media[0].file} alt={location.expand.media[0].description} />
-					<p class="m-2 text-sm">{location.expand.media[0].title}</p>
-				</div>
-			{:else if location.expand.media?.length > 1}
-				<div class="flex flex-col items-center justify-center px-16 text-center">
-					<Carousel.Root
-						setApi={(emblaApi) => (mediaAPI = emblaApi)}
-						class="flex w-fit flex-col items-center justify-center text-center"
-					>
-						<h2 class="text-md">Media</h2>
-						<Carousel.Content>
-							{#each location.expand.media || [] as media}
-								<Carousel.Item class="flex flex-col items-center justify-center">
-									<img src={media.file} alt={media.description} />
-									<p class="text-sm">{media.title}</p>
-								</Carousel.Item>
-							{/each}
-						</Carousel.Content>
-						<Carousel.Previous variant="secondary" />
-						<Carousel.Next variant="secondary" />
-					</Carousel.Root>
-					<div class="py-2 text-center text-sm text-muted-foreground">
-						{mediaCurrent} of {mediaCount}
+		<ScrollArea>
+			<div class="flex flex-col items-center">
+				<h1 class="text-xl font-bold">{location.name}</h1>
+				<p class="text-md font-normal">{location.description}</p>
+				{#if location.expand.media?.length == 1}
+					<div class="flex flex-col items-center justify-center text-center">
+						<img
+							src={location.expand.media[0].file}
+							alt={location.expand.media[0].description}
+							width="75%"
+						/>
+						<p class="m-2 text-sm">{location.expand.media[0].title}</p>
 					</div>
-				</div>
-			{/if}
-			{#if location.expand.story?.length == 1}
-				<div class="flex flex-col items-center justify-center text-center">
-					<h2 class="text-md m-2">Story</h2>
-					<img src={location.expand.story[0].field} alt={location.expand.story[0].image_title} />
-					<p class="m-2 text-sm">{location.expand.story[0].title}</p>
-					<p class="m-2 text-sm">{location.expand.story[0].text_spanish}</p>
-				</div>
-			{:else if location.expand.story?.length > 1}
-				<div class="flex flex-col items-center justify-center text-center">
-					<Carousel.Root
-						setApi={(emblaApi) => (storyAPI = emblaApi)}
-						class="flex flex-col items-center justify-center"
-					>
-						<h2 class="text-md">Stories</h2>
-						<Carousel.Content>
-							{#each location.expand.story || [] as story}
-								<Carousel.Item class="flex flex-col items-center justify-center">
-									<img src={story.field} alt={story.image_title} />
-									<p class="text-sm">{story.title}</p>
-								</Carousel.Item>
-							{/each}
-						</Carousel.Content>
-						<Carousel.Previous variant="secondary" />
-						<Carousel.Next variant="secondary" />
-					</Carousel.Root>
-					<div class="py-2 text-center text-sm text-muted-foreground">
-						{storyCurrent} of {storyCount}
+				{:else if location.expand.media?.length > 1}
+					<div class="flex flex-col items-center justify-center px-16 text-center">
+						<Carousel.Root
+							setApi={(emblaApi) => (mediaAPI = emblaApi)}
+							class="flex w-fit flex-col items-center justify-center text-center"
+						>
+							<Carousel.Content>
+								{#each location.expand.media || [] as media}
+									<Carousel.Item class="flex flex-col items-center justify-center">
+										<img src={media.file} alt={media.description} width="75%" />
+										<p class="text-sm">{media.title}</p>
+									</Carousel.Item>
+								{/each}
+							</Carousel.Content>
+							<Carousel.Previous variant="secondary" />
+							<Carousel.Next variant="secondary" />
+						</Carousel.Root>
+						<div class="py-2 text-center text-sm text-muted-foreground">
+							{mediaCurrent} of {mediaCount}
+						</div>
 					</div>
-				</div>
-			{/if}
-		</div>
+				{/if}
+				{#if location.expand.story?.length == 1}
+					<div class="flex flex-col items-center justify-center text-center">
+						<img
+							src={location.expand.story[0].field}
+							alt={location.expand.story[0].image_title}
+							width="75%"
+						/>
+						<p class="m-2 text-sm">{location.expand.story[0].title}</p>
+						<p class="m-2 text-sm">{location.expand.story[0].text_spanish}</p>
+					</div>
+				{:else if location.expand.story?.length > 1}
+					<div class="flex flex-col items-center justify-center text-center">
+						<Carousel.Root
+							setApi={(emblaApi) => (storyAPI = emblaApi)}
+							class="flex flex-col items-center justify-center"
+						>
+							<h2 class="text-md">Stories</h2>
+							<Carousel.Content>
+								{#each location.expand.story || [] as story}
+									<Carousel.Item class="flex flex-col items-center justify-center">
+										<img src={story.field} alt={story.image_title} width="75%" />
+										<p class="text-sm">{story.title}</p>
+									</Carousel.Item>
+								{/each}
+							</Carousel.Content>
+							<Carousel.Previous variant="secondary" />
+							<Carousel.Next variant="secondary" />
+						</Carousel.Root>
+						<div class="py-2 text-center text-sm text-muted-foreground">
+							{storyCurrent} of {storyCount}
+						</div>
+					</div>
+				{/if}
+			</div>
+		</ScrollArea>
 	</Dialog.Content>
 </Dialog.Root>
