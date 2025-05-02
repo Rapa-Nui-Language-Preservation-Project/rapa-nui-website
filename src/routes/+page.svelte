8899eadbed8f3d66ab9e29e87285b183863123ff
Max_Admin <max_admin@aneslap26.local>
2025-05-02 10:39:39 -0400

    added background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,37 +29,39 @@
 
 <ImageMarquee />
 
-<main class="h-screen w-screen">
-	<div class="mt-32 flex flex-col items-center justify-center">
-		<div class="m-2 w-fit rounded-xl bg-muted p-4 ">
-			<h1 class="m-2 w-fit text-wrap text-5xl min-[450px]:text-7xl">
-				<span class="font-bold">Rapa Nui</span> 
-			</h1>
-			<h2 class="m-2 mt-1 text-wrap text-2xl min-[450px]:text-4xl">Recursos para la documentación digital</h2>
+<div class="min-h-screen w-screen bg-cover bg-center bg-no-repeat" style="background-image: url('http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/oi61y31b9ukmkwa/pexels_magda_ehlers_pexels_636353_olu9cogeg5.jpg');">
+	<main class="h-screen w-screen">
+		<div class="mt-2 flex flex-col items-center justify-center">
+			<div class="m-2 w-fit rounded-xl bg-muted p-4 ">
+				<h1 class="m-2 w-fit text-wrap text-5xl min-[450px]:text-7xl">
+					<span class="font-bold">Rapa Nui</span> 
+				</h1>
+				<h2 class="m-2 mt-1 text-wrap text-2xl min-[450px]:text-4xl">Recursos para la documentación digital</h2>
+			</div>
+			<div class="m-5 mt-32 flex flex-row flex-wrap items-center justify-center gap-4">
+				<NavigationCard card={cards[0]}>
+					<img
+						src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/s718fkw89hh68y9/paper_camera2012_11_26_14_17_43_af9n9sbkio.jpg"
+						alt="imagen"
+						class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
+					/>
+				</NavigationCard>
+				<NavigationCard card={cards[1]}>
+					<img
+						src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/0de8x34jot74fcr/screenshot_2025_04_14_at_8_04_d5u5yvtdpc.27PM.png"
+						alt="An outline Map of Easter Island"
+						class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
+					/>
+				</NavigationCard>
+				<NavigationCard card={cards[2]}>
+					<img
+						src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/hv0b009265nrg85/hmppx5_13yd9imgw5.png"
+						alt=""
+						class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
+					/>
+				</NavigationCard>
+			</div>
 		</div>
-		<div class="m-5 mt-32 flex flex-row flex-wrap items-center justify-center gap-4">
-			<NavigationCard card={cards[0]}>
-				<img
-					src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/s718fkw89hh68y9/paper_camera2012_11_26_14_17_43_af9n9sbkio.jpg"
-					alt="imagen"
-					class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
-				/>
-			</NavigationCard>
-			<NavigationCard card={cards[1]}>
-				<img
-					src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/0de8x34jot74fcr/screenshot_2025_04_14_at_8_04_d5u5yvtdpc.27PM.png"
-					alt="An outline Map of Easter Island"
-					class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
-				/>
-			</NavigationCard>
-			<NavigationCard card={cards[2]}>
-				<img
-					src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/hv0b009265nrg85/hmppx5_13yd9imgw5.png"
-					alt=""
-					class="h-[200px] w-full rounded-md object-cover outline-offset-1 transition-opacity hover:outline"
-				/>
-			</NavigationCard>
-		</div>
-	</div>
-	<Footer />
-</main>
+		<Footer />
+	</main>
+</div>
