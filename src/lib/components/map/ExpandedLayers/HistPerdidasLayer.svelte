f13705212ad8402927bb53848b0424a12a57da3f
Lucas Levine <lucaslevine12@gmail.com>
2025-07-22 20:53:54 -0400

    fixed youtube embed size

--- src/lib/components/map/ExpandedLayers/HistPerdidasLayer.svelte
+++ src/lib/components/map/ExpandedLayers/HistPerdidasLayer.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-    import type { ExpandedLocation } from '$lib/expanded-models';
-    import Youtube from "svelte-youtube-embed";
-    let { location }: { location: ExpandedLocation } = $props();
+	import type { ExpandedLocation } from '$lib/expanded-models';
+	import Youtube from 'svelte-youtube-embed';
+	let { location }: { location: ExpandedLocation } = $props();
 </script>
 
 <h1 class="text-xl font-bold">{location.name}</h1>
 <p class="text-md preserve-whitespace text-center font-normal">
-    {location.description_espanol}
+	{location.description_espanol}
 </p>
-<Youtube
-  id={location.description_rapa_nui}
-  thumbnail={undefined}
-  play_button={undefined}
-/>
+<div class="h-full w-full">
+	<Youtube id={location.description_rapa_nui} thumbnail={undefined} play_button={undefined} />
+</div>
