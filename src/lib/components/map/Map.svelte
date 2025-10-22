916cc6e1db41da92054eabce82ccbd8e105ddc86
jsparr72 <sparrjack72@gmail.com>
2025-10-22 18:19:46 -0400

    Simple mobile view, possibly temporary

--- src/lib/components/map/Map.svelte
+++ src/lib/components/map/Map.svelte
@@ -10,6 +10,7 @@
 	import LeftSidebar from './sidebars/LeftSidebar.svelte';
 	import RightSidebar from './sidebars/RightSidebar.svelte';
 	import SidebarControls from './sidebars/SidebarControls.svelte';
+	import { onMount } from 'svelte';
 
 	let dark = $state(false);
 	mode.subscribe((m) => {
@@ -24,13 +25,40 @@
 	// Sidebar visibility states
 	let leftSidebarVisible = $state(true);
 	let rightSidebarVisible = $state(true);
+
+	let isDesktop = $state(true);
+
+	/* 
+	The geographic map is unnecessary for the current simple mobile view implementation.
+	The following logic automatically switches back to the Rapa Nui view in case someone
+	switches to the Geographic map while on Desktop then makes their view mobile-sized.
+
+	The main reason for doing this is b/c the Rapa Nui map image looks cleaner to display 
+	alongside the message, but also the Geographic map is broken.
+	*/
+	onMount(() => {
+		const checkScreenSize = () => {
+			isDesktop = window.innerWidth >= 768;
+		};
+		
+		checkScreenSize();
+		window.addEventListener('resize', checkScreenSize);
+		
+		return () => window.removeEventListener('resize', checkScreenSize);
+	});
+
+	$effect(() => {
+		if (!isDesktop && selectedBase === 'Geografía') {
+			selectedBase = 'Rapa Nui';
+		}
+	});
 </script>
 
-<div class="flex h-screen w-screen justify-center overflow-hidden bg-gray-100">
+<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex h-screen w-screen justify-center overflow-hidden bg-gray-100 md:ml-0">
 	<PMTilesProtocol />
 
 	<!-- Map Container -->
-	<div class="relative flex-1 bg-gray-900 transition-all duration-500 ease-in-out">
+	<div class="relative h-auto w-auto md:flex-1 bg-gray-900 transition-all duration-500 ease-in-out">
 		{#if selectedBase === 'Geografía'}
 			<MapLibre
 				class="absolute left-0 top-0 h-screen w-full"
@@ -52,26 +80,38 @@
 			{#if calibrate}
 				<CalibrationTool />
 			{:else}
-				<div class="relative ml-80 mr-64 h-screen overflow-hidden">
+				<!--Mobile view message-->
+				{#if !isDesktop}
+					<div class="absolute top text-center z-20 p-3 mt-8 bg-gradient-to-b from-amber-100 to-amber-50 border border-amber-200 rounded-lg m-4"
+						style="font-family: 'Merriweather', serif;">
+						<h3 class="text-lg font-semibold text-amber-900 mb-2">Desktop Recommended</h3>
+						<p class="text-amber-700">
+							This interactive map is optimized for desktop exploration to provide the best experience. 
+							We recommend switching to a desktop computer or tablet for full map controls and site information.
+						</p>
+					</div>
+				{/if}
+				<div class="relative h-full w-full md:ml-80 md:mr-64 md:h-screen md:w-auto overflow-hidden">
 					<img
 						src="http://127.0.0.1:8090/api/files/ia77ailu3ghoodv/6jjx168s5ezt2m8/map_k7mm569qll.png"
-						class="h-screen w-auto min-w-max"
+						class="h-full w-full md:h-screen md:w-auto object-contain md:min-w-max"
 						alt="Map of Easter Island"
 					/>
-
-					{#each selectedLayers.values() as layer}
-						{#each layer.expand.locations || [] as location}
-							{#if location.latitude != null && location.longitude != null}
-								{@const pos = transformLatLngToXY(location.latitude, location.longitude)}
-								<div
-									class="absolute z-10"
-									style={`top: ${pos.y}%; left: ${pos.x}%; transform: translate(-50%, -50%);`}
-								>
-									<LocationDialog {location} layerName={layer.name} />
-								</div>
-							{/if}
+					{#if isDesktop} <!--Conditional prevents layers from appearing on mobile view-->
+						{#each selectedLayers.values() as layer}
+							{#each layer.expand.locations || [] as location}
+								{#if location.latitude != null && location.longitude != null}
+									{@const pos = transformLatLngToXY(location.latitude, location.longitude)}
+									<div
+										class="absolute z-10"
+										style={`top: ${pos.y}%; left: ${pos.x}%; transform: translate(-50%, -50%);`}
+									>
+										<LocationDialog {location} layerName={layer.name} />
+									</div>
+								{/if}
+							{/each}
 						{/each}
-					{/each}
+					{/if}
 				</div>
 			{/if}
 		{/if}
--- src/lib/components/map/sidebars/LeftSidebar.svelte
+++ src/lib/components/map/sidebars/LeftSidebar.svelte
@@ -27,27 +27,5 @@
       
     </div>
 {:else}
-  <div class="fixed bottom-0 z-20"
-  style="font-family: 'Merriweather', serif;"
-  >
-  <Drawer.Root>
-    <Drawer.Trigger 
-    class="border border-b-2 border-r-2 border-black/10 bg-gradient-to-b from-amber-50 to-orange-50 font-serif font-bold text-amber-900 transition-all hover:text-amber-900 hover:bg-gradient-to-b hover:from-amber-100 hover:to-orange-100 py-2 px-4 rounded-lg"
-    >
-    Capas
-    </Drawer.Trigger>
-    <Drawer.Content class="bg-orange-50">
-      <Drawer.Header class="text-center text-amber-900" style="font-family: 'Merriweather', serif;">
-        <Drawer.Title>Capas</Drawer.Title>
-        <Drawer.Description class="text-amber-700" style="font-family: 'Merriweather', serif;">
-          This is where you can select layers on mobile
-        </Drawer.Description>
-      </Drawer.Header>
-      <Drawer.Footer>
-        <Button class="bg-orange-200 text-amber-700 text-md transition-all hover:bg-amber-100 " style="font-family: 'Merriweather', serif;">Submit</Button>
-        <Drawer.Close class="text-amber-700" style="font-family: 'Merriweather', serif;">Close</Drawer.Close>
-      </Drawer.Footer>
-    </Drawer.Content>
-  </Drawer.Root>
-  </div>
+  <div></div>
 {/if}
