ea9e8acfaeb24f53ec7ec6f5bbaf901a389f9569
jsparr72 <sparrjack72@gmail.com>
2025-11-06 08:38:21 -0500

    Updated desktop view to include pdf and removed sidebar titles

--- src/lib/components/map/sidebars/RightSidebar.svelte
+++ src/lib/components/map/sidebars/RightSidebar.svelte
@@ -13,13 +13,20 @@
 {#if isDesktop.current}
 	<div
 		class={`fixed right-0 top-0 z-20 h-full w-80 overflow-y-auto border-l border-black/10 bg-gradient-to-b from-amber-50 to-orange-50 font-serif text-amber-900 transition-all duration-500 ease-in-out 
-    ${visible ? 'translate-y-0 opacity-100' : 'pointer-events-none -translate-y-full opacity-0'}`}
+  ${visible ? 'translate-y-0 opacity-100' : 'pointer-events-none -translate-y-full opacity-0'}`}
 		style="font-family: 'Merriweather', serif;"
 	>
 		<div class="p-6">
 			<div class="space-y-4">
 				<BaseToggler {bases} bind:selectedBase />
 			</div>
+		</div>
+		<div
+			class="fixed bottom-0 right-0 m-4 mx-12 rounded outline-4 outline-orange-600 transition-all duration-100 ease-in-out hover:outline"
+		>
+			<a href="/InaKoMou.pdf">
+				<img src="/InaKoMouCover.png" alt="Ina Ko Mou Cover" />
+			</a>
 		</div>
 	</div>
 	<!--Mobile View-->
